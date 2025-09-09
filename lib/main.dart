599f52ac351e0d0dc38c248e0ebb316e1796217e
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2025-09-09 00:59:55 -0500

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:doublevpartnersapp/config/theme.dart';
 import 'package:doublevpartnersapp/presentation/context/context.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: "assets/.env");
   runApp(ProviderScope(child: const MainApp()));
 }
 
